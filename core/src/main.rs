54e24ecd66d523ba5ca0c357debcb68a1775e583
Ajayi Olamide Daniel <prodbyola@gmail.com>
2024-08-06 12:29:09 +0100

    Added tokio

--- core/src/main.rs
+++ core/src/main.rs
@@ -1,19 +1,42 @@
-use std::process::Command;
+use std::{io::{BufRead, BufReader, Result}, process::Command};
 
-fn convert(input: &str, output: &str) {
-    let mut cmd = Command::new("ffmmpeg");
-
-    let out = cmd
+async fn convert(input: &str, output: &str) -> std::io::Result<()> {
+    let mut cmd = Command::new("ffmmpeg")
         .args(["-i", input, output])
-        .output()
-        .expect("Unable to execute");
-    if out.status.success() {
-        println!("conversion completed")
-    } else {
-        eprintln!("conversion error: {:?}", out.stderr)
-    }
+        .stdout(std::process::Stdio::piped())
+        .stderr(std::process::Stdio::piped())
+        .spawn()?;
+
+    let out = cmd.stdout.take().unwrap();
+    let err = cmd.stderr.take().unwrap();
+
+    let out_reader = BufReader::new(out);
+    let err_reader = BufReader::new(err);
+
+    let mut out_lines = out_reader.lines();
+    let mut err_lines = err_reader.lines();
+
+    tokio::spawn(async move {
+        while let Some(line) = out_lines.next() {
+            if let Ok(line) = line {
+                println!("stdout: {}", line);
+            }
+        }
+    });
+
+    tokio::spawn(async move {
+        while let Some(line) = err_lines.next() {
+            if let Ok(line) = line {
+                eprintln!("stderr: {}", line);
+            }
+        }
+    });
+
+    Ok(())
 }
 
-fn main() {
-    convert("demo.mkv", "demo.mp4");
+#[tokio::main]
+async fn main() -> std::io::Result<()> {
+    convert("demo.mkv", "demo.mp4").await?;
+    Ok(())
 }
