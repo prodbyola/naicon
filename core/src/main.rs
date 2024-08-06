1a5e1bbe392df2b941df5dc0740d245942b072d2
Ajayi Olamide Daniel <prodbyola@gmail.com>
2024-08-06 11:25:45 +0100

    Added prototype conversion function

--- core/src/main.rs
+++ core/src/main.rs
@@ -1,3 +1,19 @@
+use std::process::Command;
+
+fn convert(input: &str, output: &str) {
+    let mut cmd = Command::new("ffmmpeg");
+
+    let out = cmd
+        .args(["-i", input, output])
+        .output()
+        .expect("Unable to execute");
+    if out.status.success() {
+        println!("conversion completed")
+    } else {
+        eprintln!("conversion error: {:?}", out.stderr)
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    convert("demo.mkv", "demo.mp4");
 }
