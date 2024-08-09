d735b53bfa65147b8fde4858789af2719a54e14d
Ajayi Olamide Daniel <prodbyola@gmail.com>
2024-08-09 17:36:56 +0100

    app(update) Animating player

--- app/composeApp/src/desktopMain/kotlin/org/dududaa/naicon/AnimatedGif.kt
+++ app/composeApp/src/desktopMain/kotlin/org/dududaa/naicon/AnimatedGif.kt
@@ -1,105 +1,235 @@
 package org.dududaa.naicon
 
-import androidx.compose.foundation.background
+import androidx.compose.animation.core.Animatable
+import androidx.compose.animation.core.tween
+import androidx.compose.foundation.Canvas
 import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.absoluteOffset
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.drawWithCache
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.draw.drawBehind
 import androidx.compose.ui.draw.drawWithContent
+import androidx.compose.ui.draw.scale
+import androidx.compose.ui.geometry.CornerRadius
 import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.geometry.Size
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.Path
+import androidx.compose.ui.graphics.StrokeCap
 import androidx.compose.ui.graphics.drawscope.Stroke
-import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.graphics.drawscope.scale
 import androidx.compose.ui.unit.dp
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+
+private val LINE_COLOR = Color.White
+private const val BOX_HEIGHT = 154
+private const val BOX_WIDTH = 216
+
+private const val IB_UNDERLAY_Y = 300f
 
 @Composable
 fun AnimatedPlayIcon() {
-    val lineColor = Color.White
-    val boxHeight = 154.dp
-    val boxWidth = 204.dp
-
     Column(verticalArrangement = Arrangement.Center) {
-        PlayerBox(lineColor, boxHeight, boxWidth)
-        PlayerArc(lineColor, boxHeight, boxWidth)
+        PlayerBox()
+        PlayerArc()
     }
 }
 
 @Composable
-private fun PlayerBox(lineColor: Color, boxHeight: Dp, boxWidth: Dp){
+private fun PlayerBox() {
     val innerBoxColor = Color.Red
 
-    val ibX = 36.dp
-    val ibY = 20.dp
+    // inner box values
+    val ibX = 40f
+    val ibY = remember { Animatable(20f) }
+    val ibHeight = remember { Animatable(BOX_HEIGHT - (ibY.value * 2)) }
+    val ibScale = remember { Animatable(1f) }
+
+    // inner box underlay values
+    val ibuY = remember { Animatable(IB_UNDERLAY_Y) }
+    val ibuPadding = 48
+
+    val hsPadding = 44f
+    val hsY = 230f // horizontal stroke y-offset
+    val hsXInit = ibuPadding + hsPadding
+    val hsX = remember { Animatable(hsXInit) }
+
+    val vsX = remember { Animatable(ibuPadding.toFloat() + 60) } // vertical stroke x-offset
 
     val borderRadius = 24.dp
     val borderWidth = 6.dp
 
-    Box(
-        modifier = Modifier
-            .height(boxHeight)
-            .width(boxWidth)
-            .border(
-                width = borderWidth,
-                color = lineColor,
-                shape = RoundedCornerShape(borderRadius)
+    val ibMove = 40f
+    val initialAnimationDuration = 1000
+    val ibScaleDuration = 250
+
+    LaunchedEffect("animationKey") {
+        launch {
+            // move inner box up
+            ibY.animateTo(-ibMove, animationSpec = tween(initialAnimationDuration))
+
+            // scale inner box out
+            ibScale.animateTo(.8f, animationSpec = tween(ibScaleDuration))
+            ibScale.animateTo(1f, animationSpec = tween(ibScaleDuration))
+
+            delay(500)
+
+            // restore inner box
+            ibY.animateTo(20f, animationSpec = tween(initialAnimationDuration))
+
+        }
+
+        launch {
+            // drag underlay down
+            ibuY.animateTo(
+                IB_UNDERLAY_Y - 180,
+                animationSpec = tween(initialAnimationDuration)
             )
 
+            delay(1000)
+
+            // drag underlay up
+            ibuY.animateTo(
+                IB_UNDERLAY_Y,
+                animationSpec = tween(initialAnimationDuration)
+            )
+
+        }
+
+        launch {
+            // expand horizontal stroke
+            hsX.animateTo(
+                hsX.value - (hsPadding - 22),
+                animationSpec = tween(initialAnimationDuration)
+            )
+
+            // move vertical stroke
+            vsX.animateTo(
+                hsX.value + 248,
+                animationSpec = tween(initialAnimationDuration)
+            )
+
+            // collapse horizontal stroke
+            hsX.animateTo(
+                hsXInit,
+                animationSpec = tween(initialAnimationDuration)
+            )
+        }
+    }
+
+    Box(
+        modifier = Modifier
+            .height(BOX_HEIGHT.dp)
+            .width(BOX_WIDTH.dp)
     ) {
+        // Outer Box
         Spacer(
             Modifier
-                .offset(ibX, ibY)
-                .background(
-                    color = innerBoxColor,
-                    shape = RoundedCornerShape(borderRadius)
-                )
                 .border(
                     width = borderWidth,
-                    color = lineColor,
+                    color = LINE_COLOR,
                     shape = RoundedCornerShape(borderRadius)
                 )
-                .drawWithCache {
+                .clip(RoundedCornerShape(12.dp))
+                .drawBehind {
+                    val width = size.width - (ibuPadding * 2)
+                    val hsWidth = size.width - hsX.value
 
-                    // play button offset
-                    val x = 92F
-                    val y = 66F
+                    val vsH = 60f // vertical stroke height
 
-                    // play button dimensions
-                    val width = size.width - (x * 2)
-                    val height = size.height - (y * 2)
+                    // draw box underlay
+                    drawRoundRect(
+                        color = LINE_COLOR.copy(alpha = .4f),
+                        topLeft = Offset(ibuPadding.toFloat(), -ibuY.value),
+                        size = Size(width = width, height = size.height),
+                        cornerRadius = CornerRadius(64f)
+                    )
 
-                    // draw play button
-                    val path = Path()
-                    path.moveTo(x, y)
-                    path.lineTo(x + width, size.height / 2)
-                    path.lineTo(x, height + y)
-                    path.close()
+                    // draw horizontal stroke
+                    drawLine(
+                        color = LINE_COLOR,
+                        start = Offset(hsX.value, hsY),
+                        end = Offset(hsWidth, hsY),
+                        strokeWidth = 18f,
+                        cap = StrokeCap.Round
+                    )
 
-                    onDrawBehind {
-                        drawPath(path, lineColor)
-                    }
+                    // draw vertical line
+                    drawLine(
+                        color = LINE_COLOR,
+                        start = Offset(vsX.value, hsY - (vsH / 2)),
+                        end = Offset(vsX.value, hsY + (vsH / 2)),
+                        strokeWidth = 18f,
+                        cap = StrokeCap.Round
+                    )
                 }
-                .width(boxWidth - (ibX * 2))
-                .height(boxHeight - (ibY * 2))
+                .fillMaxSize()
         )
+
+        // Inner Box
+        Canvas(
+            Modifier
+                .absoluteOffset(ibX.dp, ibY.value.dp)
+                .width(BOX_WIDTH.dp - (ibX.dp * 2))
+                .height(ibHeight.value.dp)
+                .scale(ibScale.value)
+        ){
+            scale(ibScale.value){
+                // box background
+                drawRoundRect(
+                    color = innerBoxColor,
+                    cornerRadius = CornerRadius(borderRadius.value + 12)
+                )
+
+                // box border
+                drawRoundRect(
+                    color = LINE_COLOR,
+                    style = Stroke(borderWidth.value * 2),
+                    cornerRadius = CornerRadius(borderRadius.value)
+                )
+
+                // play button offset
+                val x = 92F
+                val y = 66F
+
+                // play button dimensions
+                val width = size.width - (x * 2)
+                val height = size.height - (y * 2)
+
+                // draw play button
+                val path = Path()
+                path.moveTo(x, y)
+                path.lineTo(x + width, size.height / 2)
+                path.lineTo(x, height + y)
+                path.close()
+
+                drawPath(path, LINE_COLOR)
+
+
+            }
+        }
     }
 }
 
 @Composable
-private fun PlayerArc(lineColor: Color, boxHeight: Dp, boxWidth: Dp) {
+private fun PlayerArc() {
     Spacer(
         modifier = Modifier
             .drawWithContent {
                 drawArc(
-                    color = lineColor,
+                    color = LINE_COLOR,
                     startAngle = 210f,
                     sweepAngle = 120f,
                     useCenter = false,
@@ -107,7 +237,7 @@ private fun PlayerArc(lineColor: Color, boxHeight: Dp, boxWidth: Dp) {
                     topLeft = Offset(0f, -4f)
                 )
             }
-            .width(boxWidth)
-            .height(boxHeight)
+            .width(BOX_WIDTH.dp)
+            .height(BOX_HEIGHT.dp)
     )
 }
